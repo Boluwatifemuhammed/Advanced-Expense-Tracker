7feabdfd4fbdaf0423edbe766398b43a31fb5811
Boluwatife Muhammed <boluwatifemuhammed8@gmail.com>
2026-05-07 22:32:08 +0100

    started the js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-const
+const balance = document.getElementById("balance");
+const incomeAmount = document.getElementById("income-amount");
+const expenseAmount = document.getElementById("expense-amount");
+const transactionList = document.getElementById("transaction-list");
+const description = document.getElementById("description");
+const amount = document.getElementById("amount");
+const submitBtn = document.getElementById("submit-btn");
+const balance = document.getElementById("balance");
